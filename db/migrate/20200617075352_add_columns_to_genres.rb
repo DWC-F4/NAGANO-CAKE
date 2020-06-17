6f11b9b554a63c7661796bdc65892c7041650bf4
vagrant <nzm.11xz.s@gmail.com>
2020-06-17 09:13:04 +0000

    remove null:faulse from genres migration file

--- db/migrate/20200617075352_add_columns_to_genres.rb
+++ db/migrate/20200617075352_add_columns_to_genres.rb
@@ -1,6 +1,6 @@
 class AddColumnsToGenres < ActiveRecord::Migration[5.2]
   def change
-    add_column :genres, :name, :string, null: false
-    add_column :genres, :genre_status, :boolean, default: false, null: false
+    add_column :genres, :name, :string
+    add_column :genres, :genre_status, :boolean, default: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_17_044406) do
+ActiveRecord::Schema.define(version: 2020_06_17_075352) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2020_06_17_044406) do
   create_table "genres", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.boolean "genre_status", default: false
   end
 
   create_table "order_details", force: :cascade do |t|
