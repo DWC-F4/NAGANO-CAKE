290f40a53bfd158ce91d4d2706809797d84ed3c8
vagrant <vagrant@localhost.localdomain>
2020-06-17 09:14:10 +0000

    fix カラムの修正

--- db/migrate/20200617075139_add_columns_to_order_details.rb
+++ db/migrate/20200617075139_add_columns_to_order_details.rb
@@ -1,8 +1,8 @@
 class AddColumnsToOrderDetails < ActiveRecord::Migration[5.2]
   def change
-    add_column :order_details, :order_id, :integer
-    add_column :order_details, :product_id, :integer
-    add_column :order_details, :price, :integer
+    add_column :order_details, :order_id, :integer, null: false
+    add_column :order_details, :product_id, :integer, null: false
+    add_column :order_details, :price, :integer, null: false
     add_column :order_details, :quantity, :integer, default: 1, null: false
     add_column :order_details, :product_status, :integer, default: 1, null: false
   end
