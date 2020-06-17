b914c2a07bb59ecf710c83c3b5f179d174802925
vagrant <vagrant@localhost.localdomain>
2020-06-17 08:23:37 +0000

    add　order・delivery_infoカラム追加

---
+++ db/migrate/20200617073538_add_columns_to_delivery_infos.rb
@@ -0,0 +1,8 @@
+class AddColumnsToDeliveryInfos < ActiveRecord::Migration[5.2]
+  def change
+    add_column :delivery_infos, :user_id, :integer
+    add_column :delivery_infos, :address_name, :string
+    add_column :delivery_infos, :postal_code, :strng
+    add_column :delivery_infos, :address, :string
+  end
+end
