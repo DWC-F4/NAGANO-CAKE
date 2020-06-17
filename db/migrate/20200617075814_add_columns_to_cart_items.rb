c4224e14c60d72e2548bc14cf70efd7f22aa90a1
vagrant <vagrant@localhost.localdomain>
2020-06-17 09:10:05 +0000

    Fix Colums
    デフォルト値のない、カラムにnot nullをつけていたので修正しました。

--- db/migrate/20200617075814_add_columns_to_cart_items.rb
+++ db/migrate/20200617075814_add_columns_to_cart_items.rb
@@ -1,7 +1,7 @@
 class AddColumnsToCartItems < ActiveRecord::Migration[5.2]
   def change
-    add_column :cart_items, :user_id, :integer, null: false
-    add_column :cart_items, :product_id, :integer, null: false
+    add_column :cart_items, :user_id, :integer
+    add_column :cart_items, :product_id, :integer
     add_column :cart_items, :quantity, :integer, default: 1, null: false
   end
 end
