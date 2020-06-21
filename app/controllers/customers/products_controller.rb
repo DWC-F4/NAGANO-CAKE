ef01b9cab8571ba1172c7f34151600a68496c73c
vagrant <vagrant@localhost.localdomain>
2020-06-21 02:05:19 +0000

    Add 商品一覧ページ見出し追加

--- app/controllers/customers/products_controller.rb
+++ app/controllers/customers/products_controller.rb
@@ -4,8 +4,10 @@ class Customers::ProductsController < ApplicationController
 		if params[:genre_id]
 			genre = Genre.find(params[:genre_id])
 			@products = genre.products
+			@title = genre.name
 		else
 			@products = Product.all
+			@title = "商品"
 		end
 		@genres = Genre.all
 	end
