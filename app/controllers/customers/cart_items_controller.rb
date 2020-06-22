4a6b30a6e015666dd0d57a1e541b86d285eeecee
vagrant <vagrant@localhost.localdomain>
2020-06-22 01:13:52 +0000

    Add cart_items controller view
    cart_itemsコントローラーのアクションとビューを追加しました。

--- app/controllers/customers/cart_items_controller.rb
+++ app/controllers/customers/cart_items_controller.rb
@@ -1,20 +1,58 @@
 class Customers::CartItemsController < ApplicationController
 	def index
-		@products = Product.all
-		# @cart_items = @products.cart_items.id
-
+		@cart_items = current_user.cart_items
+		@sum = 0
+		@cart_items.each do |cart|
+			@sum += cart.product.price * cart.quantity
+		end
 	end
 
 	def create
+		@cart_item = CartItem.new(cart_item_params)
+		@cart_item.user_id = current_user.id
+
+		@suffered = CartItem.find_by(product_id: @cart_item.product_id, user_id: current_user.id)
+		if @suffered.nil?
+			if @cart_item.save
+				flash[:notice] = "カートに追加しました"
+				redirect_to customers_cart_items_path
+			else
+				redirect_back(fallback_location: root_path)
+			end
+		else
+			@suffered.update(cart_item_params)
+			#@suffered.update(quantity: @suffered.quantity+1)
+			# 下記を1行にまとめた記述
+			# @suffered.quantity += 1
+			# @suffered.save
+			redirect_to customers_cart_items_path
+		end
 	end
 
 	def destroy
+		cart_item = CartItem.find(params[:id])
+		cart_item.destroy
+		flash[:notice] = "カートから削除しました"
+		redirect_to customers_cart_items_path
 	end
 
 	def destroy_all
+		cart_items = current_user.cart_items
+		cart_items.destroy_all
+		flash[:notice] = "カートを空にしました"
+		redirect_to customers_cart_items_path
 	end
 
 	def update
+		cart_item = CartItem.find(params[:id])
+		cart_item.update(cart_item_params)
+		flash[:notice] = "数量を変更しました"
+		redirect_to customers_cart_items_path
+	end
+
+	private
+	def cart_item_params
+		params.require(:cart_item).permit(:user_id, :product_id, :quantity)
 	end
 
 end
