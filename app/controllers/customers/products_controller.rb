96754de97fdfebc63e1cbbdc4da7292ffbe87b02
vagrant <vagrant@localhost.localdomain>
2020-06-23 18:20:43 +0000

    Fix 顧客商品一覧に販売停止中の商品を非表示にしました

--- app/controllers/customers/products_controller.rb
+++ app/controllers/customers/products_controller.rb
@@ -3,10 +3,10 @@ class Customers::ProductsController < ApplicationController
 	def index
 		if params[:genre_id]
 			genre = Genre.find(params[:genre_id])
-			@products = genre.products
+			@products = genre.products.active
 			@title = genre.name
 		else
-			@products = Product.all
+			@products = Product.active
 			@title = "商品"
 		end
 		@genres = Genre.active
