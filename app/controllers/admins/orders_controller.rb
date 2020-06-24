4bddc8a2ac5f4f340cf01aa0e8c84583a4162787
vagrant <vagrant@localhost.localdomain>
2020-06-24 03:08:53 +0000

    Fix admins/order
    管理者側のordersコントローラー/oreder_detailsを修正しました。

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -13,6 +13,12 @@ class Admins::OrdersController < ApplicationController
 	def update
 	  @order = Order.find(params[:id])
 	  if @order.update(order_params)
+	  	if @order.order_status == "入金確認"
+	  		@order.order_details.each do |order_detail|
+	  			order_detail.product_status = "製作待ち"
+	  			order_detail.save
+	  		end
+	  	end
 	  	flash[:notice] = "注文履歴詳細が変更されました。"
 	  	redirect_to admins_order_path(@order)
 	  else
